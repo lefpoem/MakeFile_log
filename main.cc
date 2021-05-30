6a98014b5a3300fac33e2ffd6a6344e1b3107210
lem <73171983+lefpoem@users.noreply.github.com>
2021-05-30 23:25:26 +0800

    Create main.cc

---
+++ main.cc
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * @brief power - Calculate the power of the number
+ * 
+ * @param base : Base value
+ * @param exponent : Exponent value
+ * @return base raised to the power exponent
+ */
+double power(double base, int exponent)
+{
+    int result = base;
+    int i;
+    if (exponent == 0)
+    {
+        return 1;
+    }
+    for(i=1;i < exponent;++i)
+    {
+        result*=base;
+    }
+    return result;
+}
+/**
+ * @brief 
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+int main(int argc, char *argv[])
+{
+    if(argc < 3){
+        printf("Usage: %s base exponent\n",argv[0]);
+        return 1;
+    }
+    double base = atof(argv[1]);
+    int exponent = atoi(argv[2]);
+    double result=power(base,exponent);
+    printf("%g ^ %d is %g\n",base,exponent,result);
+    return 0;
+}
