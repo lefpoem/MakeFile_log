d723698648a5304c6bd8c222e0044d00380a7adc
lem <73171983+lefpoem@users.noreply.github.com>
2021-05-30 12:53:09 +0800

    Create phony.mk

---
+++ phony.mk
@@ -0,0 +1,15 @@
+all : prog1 prog2 prog3 #target can depend
+.PHONY:all
+prog1 : prog1.o utils.o
+	cc -o prog1 prog1.o utils.o
+prog2 : prog2.o
+	cc -o prog2 prog2.o
+prog3 : prog3.o short.o utils.o
+	cc -o prog3 prog3.o short.o utils.o
+.PHONY: cleanall cleanobj cleandiff #phony can depend
+cleanall : cleanobj cleandiff
+	rm program
+cleanobj : 
+	rm *.obj
+cleandiff :
+	rm *.diff
