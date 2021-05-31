bb2997d2abc04ecbc3a91c3bba2224c359469a51
lem <73171983+lefpoem@users.noreply.github.com>
2021-05-31 17:08:26 +0800

    Add files via upload

---
+++ cmake3/math/MathFunctions.cpp
@@ -0,0 +1,21 @@
+/**
+ * @brief 
+ * 
+ * @param base 
+ * @param exponent 
+ * @return double 
+ */
+double power(double base, int exponent)
+{
+    int result = base;
+    int i;
+    if (exponent == 0)
+    {
+        return 1;
+    }
+    for(i=1;i < exponent;++i)
+    {
+        result*=base;
+    }
+    return result;
+}
